b4e964b914dff6d9a96ae41b794fc820804c0888
Jaylo <jaylogalam04@gmail.com>
2025-06-03 21:21:21 +0800

    added aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": "/src/assets",
+      "@utils": "/src/utils",
+      "@components": "/src/components",
+      "@app": "/src/app",
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
